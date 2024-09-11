9c4723b658274d74099e0713ce98af1b18bbec83
Simon Widmann <64704230+Widdi97@users.noreply.github.com>
2024-09-11 20:45:12 +0200

    Making sketch generation more efficient

--- cycloid.py
+++ cycloid.py
@@ -5,14 +5,19 @@ import Sketcher
 
 # Parameters of the cycloidal disk
 
-N = 10 # number of Rollers
-R = 10.0 # [mm] radius of the Rotor
-R_r = 1.5 # [mm] radius of the Rollers
-E = 0.75 # [mm] excentricity (or offset) from the input shaft to the center of the rotor
-steps = 2000 # number of points on the parametrization
+N = 35 # number of Rollers
+R = 68 # [mm] radius of the Rotor
+R_r = 3.3 # [mm] radius of the Rollers
+E = 1.5 # [mm] excentricity (or offset) from the input shaft to the center of the rotor
+steps = 5000 # number of points on the parametrization
 
 
 debugging = False
+
+
+def rescale_func(x, factor=0.7):
+    # choose factor between 0 and 1
+    return x + factor * sin(x * (N - 1) * 2 * pi +  pi) / (2 * (N - 1) *  pi)
 
 class Epitrochoid:
     def __init__(self,R,R_r,E,N):
@@ -41,18 +46,17 @@ class EpitrochoidSketcher:
         
     def run(self):
         # generate coordinates
-        thetaList = [j / self.steps * 2 * pi for j in range(self.steps + 1)]
-        xList = [epi.x(theta) for theta in thetaList]
-        yList = [epi.y(theta) for theta in thetaList]
-        
+        # thetaList = [j / self.steps * 2 * pi for j in range(self.steps + 1)]
+        thetaList = [rescale_func(j / self.steps, 0.8) for j in range(self.steps + 1)]
+        # thetaList = [rescale_func(theta, 0.8) for theta in thetaList]
+        xList = [epi.x(theta * 2 * pi) for theta in thetaList]
+        yList = [epi.y(theta * 2 * pi) for theta in thetaList]
         # add lines to the sketch
         for j in range(self.steps):
             x1, x2 = xList[j:j+2]
             y1, y2 = yList[j:j+2]
             self.sketch.addGeometry(Part.LineSegment(App.Vector(x1, y1, 0),
                                         App.Vector(x2, y2, 0)), False)
-        for j in range(self.steps - 1):
-            self.sketch.addConstraint(Sketcher.Constraint("Coincident", j, 2, j+1, 1))
         self.sketch.addConstraint(Sketcher.Constraint("Coincident", 0, 1, self.steps - 1, 2))
         self.doc.recompute()
     
